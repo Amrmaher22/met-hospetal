4165cfed2b5f7dc14ea7560380cde06affc40085
Amrmaher22 <120342595+Amrmaher22@users.noreply.github.com>
2023-05-06 10:41:14 +0300

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,8 @@
+
+<?php
+
+header('Location: Appointment/admin/index.php');
+
+exit;
+
+?>
---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="ar">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>مستشفى الجزيرة الدولى (السلاب)</title>
+
+
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" rel="stylesheet">
+  <!-- استدعاء المكتبات الجاهزة  -->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link href="assets/css/edits.css" rel="stylesheet">
+
+  
+  <link href="assets/css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body  >
+
+  <!-- ======= الجزء العلوى الثابت  ======= -->
+  <div id="topbar" class="d-flex align-items-center fixed-top">
+    <div class="container d-flex align-items-center  " style="    text-align: center;
+    ">
+     
+      <div class="d-flex align-items-center">
+        <i class="bi bi-phone"></i> إتصل بنا الآن 0502208559 | 0502208558 | 0502208557
+ 
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= رأس الصفحة  ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt=""></a>
+     <!-- ======= الروابط | النافبار  ======= -->
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto " href="#hero">الرئيسية</a></li>
+          <li><a class="nav-link scrollto" href="#about">عن مستشفى الجزيرة الدولى </a></li>           
+          <li><a class="nav-link scrollto" href="#contact">اتصل بنا </a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="Appointment" class="appointment-btn scrollto"><span class="d-none d-md-inline">حجز </span> موعد </a>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= سلايدر ( صور متحركة )  ======= -->
+  <section id="hero">
+    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
+      <div class="carousel-inner" role="listbox">
+
+        <!-- الصورة الأولى -->
+        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
+          <div class="container">
+            <h2>مرحبا بكم  <span>فى مستشفى الجزيرة الدولى 
+              السلاب
+            </span></h2>
+            <p> مستشفى الجزيرة الدولي ( السلاب ) منذ عام 1992. مؤسسة خدمية متخصصة في تقديم الخدمات الطبية المتميزة لأبناء محافظات شرق الدلتا والمحافظات المجاورة خاصة وأبناء جمهورية مصر العربية والدول العربية والإسلامية عامة
+
+            </p>
+            <a href="#about" class="btn-get-started scrollto">إقرأ أكثر عنا </a>
+          </div>
+        </div>
+
+        <!-- الصورة الثانية -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
+          <div class="container">
+            <h2>مرحبا بكم  <span>فى مستشفى الجزيرة الدولى 
+              السلاب
+            </span></h2>
+            <p> مستشفى الجزيرة الدولي ( السلاب ) منذ عام 1992. مؤسسة خدمية متخصصة في تقديم الخدمات الطبية المتميزة لأبناء محافظات شرق الدلتا والمحافظات المجاورة خاصة وأبناء جمهورية مصر العربية والدول العربية والإسلامية عامة
+
+            </p>          </div>
+        </div>
+
+        <!-- الصورة الثالثة  -->
+        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+          <div class="container">
+            <h2>مرحبا بكم  <span>فى مستشفى الجزيرة الدولى 
+              السلاب
+            </span></h2>
+            <p> مستشفى الجزيرة الدولي ( السلاب ) منذ عام 1992. مؤسسة خدمية متخصصة في تقديم الخدمات الطبية المتميزة لأبناء محافظات شرق الدلتا والمحافظات المجاورة خاصة وأبناء جمهورية مصر العربية والدول العربية والإسلامية عامة
+
+            </p>          </div>
+        </div>
+          <!-- الصورة الرابعة   -->
+          <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
+            <div class="container">
+              <h2>مرحبا بكم  <span>فى مستشفى الجزيرة الدولى 
+                السلاب
+              </span></h2>
+              <p> مستشفى الجزيرة الدولي ( السلاب ) منذ عام 1992. مؤسسة خدمية متخصصة في تقديم الخدمات الطبية المتميزة لأبناء محافظات شرق الدلتا والمحافظات المجاورة خاصة وأبناء جمهورية مصر العربية والدول العربية والإسلامية عامة
+  
+              </p>          </div>
+          </div>
+
+      </div>
+
+      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+      </a>
+
+      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+      </a>
+
+    </div>
+  </section><!-- End Hero -->
+
+   
+
+  <!-- ======= ذيل الصفحة  ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6">
+            <div class="footer-info">
+              <h3>مستشفى السلاب الدولى </h3>
+              <p>
+                مصر , الدقهلية, المنصورة
+ <br>
+               <br><br>
+                <strong>Phone:</strong> 2208557 050
+                <br>
+                <strong>Email:</strong> info@elsallabhospitals.com
+                <br>
+              </p>
+              <div class="social-links mt-3">
+                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4> </h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">الرئيسة</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">عن مستشفى الجزيرة الدولى </a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">الخدمات </a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">الأطباء</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>الأقسام</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">قسم جراحة القلب</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">قسم جراحة المخ والأعصاب</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">قسم أمراض الكلى</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">قسم العناية المركزة</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">قسم النساء والتوليد </a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>تابعنا</h4>
+             <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; 2023  <strong><span>لمستشفى السلاب الدولى </span></strong>. جميع الحقوق محفوظة 
+      </div>
+     
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- استدعاء مكتبات الجافا سكريبت  -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+    
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
